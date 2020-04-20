4f07a0af350df152bf8016fbb523c30052694074
surendra-bitwerks <surendra.bitwerks@gmail.com>
2020-04-20 10:46:59 +0545

    Added enum parameters

--- RFID/RFID/main.c
+++ RFID/RFID/main.c
@@ -11,6 +11,7 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdbool.h>
 
 #include "Serial.h"
 #include "spi.h"
@@ -27,9 +28,50 @@
 #define CS_LOW()					PORT_CS &=~ (1 << CS_PIN)
 #define CS_HIGH()					PORT_CS |= (1 << CS_PIN)
 
+#define nullptr ((void*)0)
 
-typedef enum   {
+typedef enum {
+	STATUS_OK				,	// Success
+	STATUS_ERROR			,	// Error in communication
+	STATUS_COLLISION		,	// Collission detected
+	STATUS_TIMEOUT			,	// Timeout in communication.
+	STATUS_NO_ROOM			,	// A buffer is not big enough.
+	STATUS_INTERNAL_ERROR	,	// Internal error in the code. Should not happen ;-)
+	STATUS_INVALID			,	// Invalid argument.
+	STATUS_CRC_WRONG		,	// The CRC_A does not match
+	STATUS_MIFARE_NACK		= 0xff	// A MIFARE PICC responded with NAK.
+	}StatusCode;
+	
+typedef enum {
+	// The commands used by the PCD to manage communication with several PICCs (ISO 14443-3, Type A, section 6.4)
+	PICC_CMD_REQA			= 0x26,		// REQuest command, Type A. Invites PICCs in state IDLE to go to READY and prepare for anticollision or selection. 7 bit frame.
+	PICC_CMD_WUPA			= 0x52,		// Wake-UP command, Type A. Invites PICCs in state IDLE and HALT to go to READY(*) and prepare for anticollision or selection. 7 bit frame.
+	PICC_CMD_CT				= 0x88,		// Cascade Tag. Not really a command, but used during anti collision.
+	PICC_CMD_SEL_CL1		= 0x93,		// Anti collision/Select, Cascade Level 1
+	PICC_CMD_SEL_CL2		= 0x95,		// Anti collision/Select, Cascade Level 2
+	PICC_CMD_SEL_CL3		= 0x97,		// Anti collision/Select, Cascade Level 3
+	PICC_CMD_HLTA			= 0x50,		// HaLT command, Type A. Instructs an ACTIVE PICC to go to state HALT.
+	PICC_CMD_RATS           = 0xE0,     // Request command for Answer To Reset.
+	// The commands used for MIFARE Classic (from http://www.mouser.com/ds/2/302/MF1S503x-89574.pdf, Section 9)
+	// Use PCD_MFAuthent to authenticate access to a sector, then use these commands to read/write/modify the blocks on the sector.
+	// The read/write commands can also be used for MIFARE Ultralight.
+	PICC_CMD_MF_AUTH_KEY_A	= 0x60,		// Perform authentication with Key A
+	PICC_CMD_MF_AUTH_KEY_B	= 0x61,		// Perform authentication with Key B
+	PICC_CMD_MF_READ		= 0x30,		// Reads one 16 byte block from the authenticated sector of the PICC. Also used for MIFARE Ultralight.
+	PICC_CMD_MF_WRITE		= 0xA0,		// Writes one 16 byte block to the authenticated sector of the PICC. Called "COMPATIBILITY WRITE" for MIFARE Ultralight.
+	PICC_CMD_MF_DECREMENT	= 0xC0,		// Decrements the contents of a block and stores the result in the internal data register.
+	PICC_CMD_MF_INCREMENT	= 0xC1,		// Increments the contents of a block and stores the result in the internal data register.
+	PICC_CMD_MF_RESTORE		= 0xC2,		// Reads the contents of a block into the internal data register.
+	PICC_CMD_MF_TRANSFER	= 0xB0,		// Writes the contents of the internal data register to a block.
+	// The commands used for MIFARE Ultralight (from http://www.nxp.com/documents/data_sheet/MF0ICU1.pdf, Section 8.6)
+	// The PICC_CMD_MF_READ and PICC_CMD_MF_WRITE can also be used for MIFARE Ultralight.
+	PICC_CMD_UL_WRITE		= 0xA2		// Writes one 4 byte page to the PICC.
+}PICC_Command;
+
+
+typedef enum  {
 	// Page 0: Command and status
+	//						  0x00			// reserved for future use
 	CommandReg				= 0x01 << 1,	// starts and stops command execution
 	ComIEnReg				= 0x02 << 1,	// enable and disable interrupt request control bits
 	DivIEnReg				= 0x03 << 1,	// enable and disable interrupt request control bits
@@ -99,12 +141,37 @@ typedef enum   {
 	// 						  0x3D			// reserved for production tests
 	// 						  0x3E			// reserved for production tests
 	// 						  0x3F			// reserved for production tests
-}PCD_Register;
+	}PCD_Register;
+	
+// MFRC522 commands. Described in chapter 10 of the datasheet.
+typedef enum {
+	PCD_Idle				= 0x00,		// no action, cancels current command execution
+	PCD_Mem					= 0x01,		// stores 25 bytes into the internal buffer
+	PCD_GenerateRandomID	= 0x02,		// generates a 10-byte random ID number
+	PCD_CalcCRC				= 0x03,		// activates the CRC coprocessor or performs a self-test
+	PCD_Transmit			= 0x04,		// transmits data from the FIFO buffer
+	PCD_NoCmdChange			= 0x07,		// no command change, can be used to modify the CommandReg register bits without affecting the command, for example, the PowerDown bit
+	PCD_Receive				= 0x08,		// activates the receiver circuits
+	PCD_Transceive 			= 0x0C,		// transmits data from FIFO buffer to antenna and automatically activates the receiver after transmission
+	PCD_MFAuthent 			= 0x0E,		// performs the MIFARE standard authentication as a reader
+	PCD_SoftReset			= 0x0F		// resets the MFRC522
+}PCD_Command;
 
 void PCD_WriteRegister(	PCD_Register reg,	uint8_t value);
+void PCD_WriteRegister_N(PCD_Register reg, uint8_t count, uint8_t *values);
 void PCD_init(void);
 uint8_t PCD_ReadRegister( PCD_Register reg);
+void PCD_ReadRegister_N(PCD_Register reg, uint8_t count, uint8_t *values, uint8_t rxAlign);
 void PCD_AntennaOn(void);
+void PCD_ClearRegisterBitMask(PCD_Register reg, uint8_t mask);
+void PCD_SetRegisterBitMask(PCD_Register reg, uint8_t mask);
+bool PICC_is_new_card_present(void);
+
+
+StatusCode PICC_RequestA(uint8_t *bufferATQA, uint8_t *bufferSize);
+StatusCode PICC_REQA_or_WUPA(uint8_t command, uint8_t *bufferATQA, uint8_t *bufferSize);
+StatusCode PCD_TransceiveData(uint8_t *sendData, uint8_t sendLen, uint8_t *backData, uint8_t *backLen, uint8_t *validBits /* = nullptr*/, uint8_t rxAlign/* = 0*/, bool checkCRC/* = false*/);
+StatusCode PCD_CommunicateWithPICC(uint8_t command, uint8_t waitIRq, uint8_t *sendData, uint8_t sendLen, uint8_t *backData /*= nullptr*/, uint8_t *backLen /*= nullptr*/, uint8_t *validBits /*= nullptr*/, uint8_t rxAlign /*= 0*/, bool checkCRC /*= false*/);
 
 int 
 main(void)
@@ -112,13 +179,24 @@ main(void)
 	uart_init();
 	spi_init();
 	
-	uart_transmit_string("RFID TEST\r\n");	
-	_delay_ms(1000);	
-	PCD_init();
-
-	while(1)
-	{
-	}  
+	uart_transmit_string ("Initializing PCD\r\n");
+	PCD_init ();
+	
+	_delay_ms (4);
+	uart_transmit_string ("Scan PICC to see UID, SAK, type and data blocks... \r\n");
+	
+	
+	
+    while (1) 
+    {
+		if( ! PICC_is_new_card_present ())
+		{
+			uart_transmit_string("Card_not_present\r\n");
+			continue;
+		}
+		
+		uart_transmit_string("MAIN\r\n");
+    }
 }
 
 void
@@ -131,7 +209,21 @@ PCD_WriteRegister(	PCD_Register reg,	uint8_t value)
 	
 }
 
-uint8_t 
+void PCD_WriteRegister_N(	PCD_Register reg,	///< The register to write to. One of the PCD_Register enums.
+							uint8_t count,			///< The number of bytes to write to the register
+							uint8_t *values		///< The values to write. Byte array.
+						)
+{
+	CS_LOW();
+	spi_send_byte (reg);
+	for (uint8_t index = 0; index < count; index++)
+	{
+		spi_send_byte (values[index]);
+	}
+	CS_HIGH();
+}
+
+uint8_t
 PCD_ReadRegister( PCD_Register reg)
 {
 	uint8_t value;
@@ -142,7 +234,48 @@ PCD_ReadRegister( PCD_Register reg)
 	CS_HIGH();
 	//uart_transmit_string("value received\r\n");	
 	return value;
+}
 
+void PCD_ReadRegister_N(PCD_Register reg,	///< The register to read from. One of the PCD_Register enums.
+						uint8_t count,			///< The number of bytes to read
+						uint8_t *values,		///< Byte array to store the values in.
+						uint8_t rxAlign		///< Only bit positions rxAlign..7 in values[0] are updated.
+					   ) 
+{
+	if (count == 0)
+	{
+		return;
+	}
+	
+	uint8_t address = 0x80 | reg;
+	uint8_t index = 0;
+	
+	CS_LOW();
+	count--;
+	spi_send_byte (address);				// Tell MFRC522 which address we want to read
+	
+	if (rxAlign)				// Only update bit positions rxAlign..7 in values[0]
+	{
+		// Create bit mask for bit positions rxAlign..7
+		uint8_t mask = (0xFF << rxAlign) & 0xFF;
+		
+		uint8_t value = spi_receive_byte ();
+		
+		values[0] = (values[0] & ~mask) | (value & mask);
+		index++;
+		spi_send_byte (address);
+	}
+	
+	while (index < count)
+	{
+		values[index] = spi_receive_byte ();
+		index++;
+		spi_send_byte (address);
+	}
+	
+	values[index] = spi_receive_byte ();
+	
+	CS_HIGH();	
 }
 
 void 
@@ -183,3 +316,187 @@ PCD_init(void)
 	PCD_WriteRegister(ModeReg, 0x3D);		// Default 0x3F. Set the preset value for the CRC coprocessor for the CalcCRC command to 0x6363 (ISO 14443-3 part 6.2.4)
 	PCD_AntennaOn();
 }
+
+void PCD_SetRegisterBitMask(PCD_Register reg, uint8_t mask)
+{
+	uint8_t temp;
+	temp = PCD_ReadRegister(reg);
+	PCD_WriteRegister( reg, temp | mask);
+}
+
+
+void PCD_ClearRegisterBitMask(PCD_Register reg, uint8_t mask)
+{
+	uint8_t temp;
+	temp = PCD_ReadRegister(reg);
+	PCD_WriteRegister(reg, temp & (~mask));
+}
+
+bool PICC_is_new_card_present(void)
+{
+	uint8_t bufferATQA[2];
+	uint8_t bufferSize = sizeof(bufferATQA);
+	
+	// Reset baud rates
+	PCD_WriteRegister(TxModeReg, 0x00);
+	PCD_WriteRegister(RxModeReg, 0x00);
+	// Reset ModWidthReg
+	PCD_WriteRegister(ModWidthReg, 0x26);
+	
+	StatusCode result = PICC_RequestA(bufferATQA, &bufferSize);
+	return( (result == STATUS_OK) || (result == STATUS_COLLISION) );	
+}
+
+
+StatusCode PICC_RequestA(uint8_t *bufferATQA, uint8_t *bufferSize)
+{
+	return PICC_REQA_or_WUPA (PICC_CMD_REQA, bufferATQA, bufferSize);
+}
+
+StatusCode PICC_REQA_or_WUPA (uint8_t command, uint8_t *bufferATQA, uint8_t *bufferSize)
+{
+	uint8_t validBits;
+	StatusCode status;
+	
+	if (bufferATQA == nullptr || *bufferSize < 2)			// The ATQA response is 2 bytes long.
+	{
+		return STATUS_NO_ROOM;
+	}
+	
+	PCD_ClearRegisterBitMask (CollReg, 0x80);				// ValuesAfterColl=1 => Bits received after collision are cleared.
+	validBits = 7;											// For REQA and WUPA we need the short frame format - transmit only 7 bits of the last (and only) byte. TxLastBits = BitFramingReg[2..0]
+	status = PCD_TransceiveData (&command, 1, bufferATQA, bufferSize, &validBits, 0, false);
+	if (status != STATUS_OK)
+	{
+		return status;
+	}
+	
+	if (*bufferSize != 2 || validBits != 0)
+	{
+		return STATUS_ERROR;
+	}
+	return	STATUS_OK;
+	
+}
+
+
+
+StatusCode PCD_TransceiveData(uint8_t *sendData, 
+							  uint8_t sendLen, 
+							  uint8_t *backData, 
+							  uint8_t *backLen, 
+							  uint8_t *validBits /*= nullptr*/, 
+							  uint8_t rxAlign /*= 0*/, 
+							  bool checkCRC/* = false*/)
+{
+	uint8_t waitIRq = 0x30;
+	return PCD_CommunicateWithPICC (PCD_Transceive, waitIRq, sendData, sendLen, backData, backLen, validBits, rxAlign, checkCRC);
+}
+
+
+StatusCode 
+PCD_CommunicateWithPICC(uint8_t command, uint8_t waitIRq, uint8_t *sendData, 
+								   uint8_t sendLen, uint8_t *backData /*= nullptr*/, uint8_t *backLen /*= nullptr*/, 
+								   uint8_t *validBits /*= nullptr*/, uint8_t rxAlign /*= 0*/,bool checkCRC /*= false*/)
+{
+	uint8_t txLastBits = validBits ? *validBits : 0;
+	uint8_t bitFraming = (rxAlign << 4) + txLastBits;			// RxAlign = BitFramingReg[6..4]. TxLastBits = BitFramingReg[2..0]
+	
+	PCD_WriteRegister (CommandReg, PCD_Idle);					// Stop any active command.
+	PCD_WriteRegister(ComIrqReg, 0x7F);						// Clear all seven interrupt request bits
+	PCD_WriteRegister(FIFOLevelReg, 0x80);					// FlushBuffer = 1, FIFO initialization
+	
+	PCD_WriteRegister_N (FIFODataReg, sendLen, sendData);	// Write sendData to the FIFO
+	PCD_WriteRegister(BitFramingReg, bitFraming);			// Bit adjustments
+	PCD_WriteRegister(CommandReg, command);				// Execute the command
+	
+	if (command == PCD_Transceive)
+	{
+		PCD_SetRegisterBitMask (BitFramingReg, 0x80);
+	}
+	
+	// Wait for the command to complete.
+	// In PCD_Init() we set the TAuto flag in TModeReg. This means the timer automatically starts when the PCD stops transmitting.
+	// Each iteration of the do-while-loop takes 17.86?s.
+	// TODO check/modify for other architectures than Arduino Uno 16bit
+	
+	uint16_t i;
+	for (i = 2000; i > 0; i--)
+	{
+		uint8_t n = PCD_ReadRegister (ComIrqReg);
+		if (n & waitIRq)
+		{
+			break;
+		}
+		if (n & 0x01)
+		{
+			return STATUS_TIMEOUT;
+		}
+	}
+	
+	if (i == 0)
+	{
+		return STATUS_TIMEOUT;
+	}
+	
+	uint8_t errorRegValue = PCD_ReadRegister (ErrorReg);
+	if (errorRegValue & 0x13)
+	{
+		return STATUS_ERROR;
+	}
+	
+	uint8_t _validBits = 0;
+	
+	if (backData && backLen)
+	{
+		uint8_t n = PCD_ReadRegister (FIFOLevelReg);
+		if (n > *backLen)
+		{
+			return STATUS_NO_ROOM;
+		}
+		*backLen = n;
+		
+		PCD_ReadRegister_N (FIFODataReg, n, backData, rxAlign);
+		_validBits = PCD_ReadRegister (ControlReg) & 0x07;
+		if (validBits)
+		{
+			*validBits = _validBits;
+		}
+		
+		//Tell about collisions
+		
+		if (errorRegValue & 0x08)
+		{
+			return STATUS_COLLISION;
+		}
+		
+		//Perform CRC_A validation if requested.
+		if (backData && backLen && checkCRC)
+		{
+			// In this case a MIFARE Classic NAK is not OK.
+			if (*backLen == 1 && _validBits == 4)
+			{
+				return STATUS_MIFARE_NACK;
+			}
+			// We need at least the CRC_A value and all 8 bits of the last byte must be received.
+			if (*backLen < 2 || _validBits != 0) {
+				return STATUS_CRC_WRONG;
+			}
+			// Verify CRC_A - do our own calculation and store the control in controlBuffer.
+			//uint8_t controlBuffer[2];
+			
+			/*StatusCode status = PCD_CalculateCRC(&backData[0], *backLen - 2, &controlBuffer[0]);
+			if (status != STATUS_OK) {
+				return status;
+			}
+			if ((backData[*backLen - 2] != controlBuffer[0]) || (backData[*backLen - 1] != controlBuffer[1])) {
+				return STATUS_CRC_WRONG;
+			}*/
+		}
+		
+	}
+	return STATUS_OK;
+}
+
+
+
